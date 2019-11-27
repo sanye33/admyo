33e7ed66e613cec4b0bb7ff8e99f507ff5781635
longzhou <longzhou@heytea.com>
2019-11-27 19:16:08 +0800

    feat: 增加ant

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Button>12312</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
