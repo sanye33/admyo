13ddcb0d06cb82e98501f6e6cd0804ce6ba67842
longzhouLab <longzhou@heytea.com>
2019-11-28 23:59:27 +0800

    feate: router interface

--- src/adapter/Arouter.tsx
+++ src/adapter/Arouter.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react';
+import { Route } from 'react-router-dom';
 
-export default function Arouter () {
+interface IRouters {
+  name: string;
+  path: string;
+  exact?: boolean;
+  component: React.Component;
+  childrens?: IRouters[]
+}
+
+interface IProps {
+  routers: IRouters[]
+}
+
+export default function Arouter (props: IProps) {
   return (
     <div>rtouer</div>
   )
