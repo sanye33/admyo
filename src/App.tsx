60cacd460f01cd52c3678bf363958c937bcb217e
longzhouLab <longzhou@heytea.com>
2019-11-28 23:57:05 +0800

    feat: rotuer update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from  'react-router-dom'
-import Abutton from './components/abutton'
+import Index from './pages/index'
+import NotFound from './pages/404'
+
+const router = [
+  { 
+    name: 12,
+    path: '/index',
+    component: Index
+  }, {
+    name: '404',
+    path: '/404',
+    component: NotFound
+  }
+]
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Route path='/button' component={Abutton}></Route>
+      {
+        router.map(route => {
+          return <Route key={route.name} path={route.path} component={route.component}></Route>
+        })
+      }
     </Router> 
   );
 }
